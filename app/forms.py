faff44762427b4070321515471bd6f9c0924659a
MaxiB0nng <maxgravgaard@icloud.com>
2025-05-13 09:03:33 +0200

    start

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, DecimalField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class Q1_Form(FlaskForm):
+    valg = StringField('valg', validators=[DataRequired()])
+    submit = SubmitField('svar')
+
+class Q2_Form(FlaskForm):
+    pizza = StringField('pizza')
+    submit = SubmitField('svar')
+
+class Q3_Form(FlaskForm):
+    submit = SubmitField('Svar_button')    
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('login')
---
+++ main.py
@@ -0,0 +1,42 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect
+from app.config import Config
+from app.forms import Q1_Form, Q2_Form, Q3_Form
+db = 'database.db'
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    # Her kan fx hentes data og sættes ind i html-koden
+    txt = "M.O.W."
+    return render_template('index.html', title=txt)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.run(debug=True) #Koer kun paa localhost
+    app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/q1/', methods=['POST', 'GET'])
+def q1():
+    q1_form = Q1_Form()
+    if q1_form.validate_on_submit():
+        if q1_form.valg.data:
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+            valg = q1_form.valg.data
+            print(valg)
+            cursor.execute('INSERT INTO driver(result) VALUES ('+valg+')')
+            conn.commit()
+            conn.close()
+            """
+            Ekstra-opgave:
+            En måde at sikre sig at brugeren ikke kan stemme to
+            gange, er ved at sætte en cookie her v.hj.a.javascript.
+            https://www.w3schools.com/js/js_cookies.asp
+
+            """
+            return redirect('/')
+    return render_template('q1.html', q1_form = q1_form)
